873daee16ea3251078cbcd7ac884bf3440f1ec08
Kelvin Kamau <124822949+qamaukelvin@users.noreply.github.com>
2024-08-20 10:20:00 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 
+document.getElementById('switch-to-login').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('sign-up').classList.remove('active');
+    document.getElementById('sign-up').classList.add('inactive');
+
+    setTimeout(() => {
+      document.getElementById('sign-up').classList.remove('inactive');
+      document.getElementById('log-in').classList.add('active');
+    }, 500);
+  });
+  
+  document.getElementById('switch-to-signup').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('log-in').classList.remove('active');
+    document.getElementById('log-in').classList.add('inactive');
+
+    setTimeout(() => {
+      document.getElementById('log-in').classList.remove('inactive');
+      document.getElementById('sign-up').classList.add('active');
+    }, 500);
+  });
+  
